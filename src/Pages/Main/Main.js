c6d92023dca6d37c859fd914b57e8cc30fee38c8
mr.ginger <tkg78912@gmail.com>
2021-06-08 17:55:29 +0900

    Add : footer 레이아웃 1차 완료

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import Footer from '../../Component/FooterComponent/Footer';
 
+class Main extends React.Component {
+  render() {
+    return <Footer />;
+  }
+}
+
+export default Main;
