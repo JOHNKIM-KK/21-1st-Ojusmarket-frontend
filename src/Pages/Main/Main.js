f118eeea19280a4e0dd57e4e48e97b8f9daaec3a
김민기 <mingikim@gimmingiui-MacBookAir.local>
2021-06-09 18:35:43 +0900

    Conflict 2차 수정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../../Component/HeaderComponent/Header';
-import Footer from '../../Component/FooterComponent/Footer';
 
 class Main extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <Footer />
       </>
     );
   }
