5b3ca88d9ff05f13800e941a66ff22f63fb64068
김민기 <mingikim@gimmingiui-MacBookAir.local>
2021-06-10 16:50:18 +0900

    Add : 상품상세페이지 1차 완료

--- src/Pages/ProductInfo/ProductInfo.js
+++ src/Pages/ProductInfo/ProductInfo.js
@@ -1,67 +1,113 @@
 import React from 'react';
+import Header from '../../Component/HeaderComponent/Header';
+import Footer from '../../Component/FooterComponent/Footer';
 import './ProductInfo.scss';
 
 class ProductInfo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 1,
+      infoData: {},
+      isLoading: false,
+    };
+  }
+
+  handleCount = event => {
+    const { count } = this.state;
+    const { className } = event.target;
+    if (className === 'plus') {
+      this.setState({ count: count + 1 });
+    } else if (className === 'minus' && count > 1) {
+      this.setState({ count: count - 1 });
+    }
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:3000/Data/infoData.json', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ infoData: data, isLoading: true });
+      });
+  }
+
   render() {
+    const { count, infoData, isLoading } = this.state;
     return (
-      <div className="contents-container">
-        <div className="img-container">
-          <span>
-            <a href="#">홈</a>
-            {'>'}
-            <a href="#">오감동</a>
-            {'>'}
-            <a href="#">청과</a>
-          </span>
-          <div className="image">암튼 무야호</div>
-        </div>
-        <div className="info-container">
-          <div className="text-title">
-            <h1>유기농 무야호</h1>
-          </div>
-          <div className="text-contents">
-            <div className="price">
-              <span className="price-text">일반가</span>
-              <span className="price-number">3,800원</span>
+      isLoading && (
+        <>
+          <Header />
+          <div className="contents-container">
+            <div className="img-container">
+              <span>
+                <a href="#">홈</a>
+                {'>'}
+                <a href="#">오감동</a>
+                {'>'}
+                <a href="#">청과</a>
+              </span>
+              <img className="image" src={infoData.image_url} />
             </div>
-            <div className="info-box">
-              <div className="delivery">
-                <span>배송비</span>
-                <span>3,000원</span>
+            <div className="info-container">
+              <div className="text-title">
+                <h1>{infoData.name}</h1>
               </div>
-              <div className="storage">
-                <span>보관방법</span>
-                <span>씽씽</span>
+              <div className="text-contents">
+                <div className="price">
+                  <span className="price-text">일반가</span>
+                  <span className="price-number">
+                    {infoData.price.toLocaleString()}원
+                  </span>
+                </div>
+                <div className="info-box">
+                  <div className="delivery">
+                    <span>배송비</span>
+                    <span>3,000원</span>
+                  </div>
+                  <div className="storage">
+                    <span>보관방법</span>
+                    <span>{infoData.storage}</span>
+                  </div>
+                </div>
+              </div>
+              <div className="product-feature__box">
+                <span>상품특징</span>
+                <span>{infoData.info}</span>
+              </div>
+              <div className="product-counter">
+                <span className="buy-counter">구매수량</span>
+                <div className="count-button">
+                  <div className="button-container">
+                    <button className="minus" onClick={this.handleCount}>
+                      -
+                    </button>
+                    <span className="counter">{count}</span>
+                    <button className="plus" onClick={this.handleCount}>
+                      +
+                    </button>
+                  </div>
+                  <div className="total-price">
+                    <span>{`${(
+                      infoData.price * count
+                    ).toLocaleString()}`}</span>
+                    <span>원</span>
+                  </div>
+                </div>
+              </div>
+              <div className="btn-area">
+                <button>장바구니</button>
+                <button>바로구매</button>
+                <button>
+                  <i className="far fa-heart"></i>
+                </button>
               </div>
             </div>
           </div>
-          <div className="product-feature__box">
-            <span>상품특징</span>
-            <span>바나나로 키워 부드럽고 달콤한 근데 이제 마크업을 곁들인</span>
-          </div>
-          <div className="product-counter">
-            <span className="buy-counter">구매수량</span>
-            <div className="count-button">
-              <div className="button-container">
-                <button className="minus">-</button>
-                <span className="counter">1</span>
-                <button className="plus">+</button>
-              </div>
-              <div className="total-price">
-                <span>3800</span>
-                <span>원</span>
-              </div>
-            </div>
-          </div>
-          <div className="btn-area">
-            <button>장바구니</button>
-            <button>바로구매</button>
-            <button>
-              <i class="far fa-heart"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+          <Footer />
+        </>
+      )
     );
   }
 }
