6b0556610345bd5cf452f68a994500cc20b8fc47
LEEKIWAN <guy2895@gmail.com>
2021-06-11 19:48:36 +0900

    Add : 회원가입 로직 구현 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,10 +6,87 @@ import Footer from '../../Component/FooterComponent/Footer';
 import './Login.scss';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      identity: '',
+      password: '',
+      background: '#d2f7d2',
+      disabled: true,
+      idChecked: false,
+      pwChecked: false,
+      isAvailedInfo: '',
+    };
+  }
+  //로그인 버튼활성화
+  // handleInputId = e => {
+  //   this.setState({ identity: e.target.value });
+  //   e.target.value.length > 4
+  //     ? this.setState({ idChecked: true }, () => this.btnChangeColor())
+  //     : this.setState({ idChecked: false }, () => this.btnChangeColor());
+  // };
+
+  // handleInputPw = e => {
+  //   this.setState({ password: e.target.value });
+  //   e.target.value.length > 7
+  //     ? this.setState({ password: e.target.value, pwChecked: true }, () =>
+  //         this.btnChangeColor()
+  //       )
+  //     : this.setState({ pwChecked: false }, () => this.btnChangeColor());
+  // };
+  handleInput = e => {
+    // input에 작성한 값
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  colorChange = () => {
+    if (this.state.identity.length > 4 && this.state.password.length > 4) {
+      this.setState({
+        background: '#6ca437',
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        background: '#d2f7d2',
+        disabled: true,
+      });
+    }
+  };
+  // if(validation = 'true')
+
+  // btnChangeColor = () => {
+  //   this.state.idChecked && this.state.pwChecked
+  //     ? this.setState({ background: '#6ca437', disabled: false })
+  //     : this.setState({ background: '#d2f7d2', disabled: true });
+  // };
+  //버튼 이벤트 (백엔드 통신)
+  goToMain = () => {
+    fetch('http://10.58.7.179:8000/user/sign-in', {
+      method: 'POST',
+      body: JSON.stringify({
+        identity: this.state.identity,
+        password: this.state.password,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log('결과: ', result);
+        //객체속 acess_token 값이 주어지면 로그인성공으로 페이지 이동
+        if (result.acess_token) {
+          alert('로그인 성공');
+          this.props.history.push('/main');
+        }
+      });
+  };
+  //회원가입 버튼 링크
   goToSignUp = () => {
     this.props.history.push('/signup');
   };
   render() {
+    let validation =
+      this.state.identity.length > 4 && this.state.password.length > 4;
     return (
       <div>
         <nav className="navi">
@@ -26,17 +103,33 @@ class Login extends Component {
 
                   <form className="form">
                     <div className="input_box">
-                      <input id="id" type="text" placeholder="아이디" />
+                      <input
+                        id="id"
+                        onChange={this.handleInput}
+                        onKeyUp={this.colorChange}
+                        name="identity"
+                        type="text"
+                        placeholder="아이디"
+                      />
                     </div>
                     <div className="input_box">
                       <input
                         id="password"
+                        onChange={this.handleInput}
+                        onKeyUp={this.colorChange}
+                        name="password"
                         type="password"
                         placeholder="비밀번호"
                       />
                     </div>
                     <div className="button_box">
-                      <button type="button" className="btn">
+                      <button
+                        style={{ backgroundColor: this.state.background }}
+                        disabled={this.state.disabled}
+                        onClick={this.goToMain}
+                        type="button"
+                        className="btn"
+                      >
                         <span>로그인</span>
                       </button>
                     </div>
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -14,18 +14,24 @@ class Signup extends Component {
       disabled: true,
       background: '#d2f7d2',
       password: '',
+      repassword: '',
       isAvailedPassword: '',
       name: '',
       phone: '',
+      isAvailedPhone: '',
       email: '',
-      address: '',
-      zoneCode: '',
-      fullAddress: '',
+      isAvailedEmail: '',
+      // address: '서울시',
+      // zoneCode: '',
+      firstAddress: '',
+      secondAddress: '',
       isDaumPost: false,
     };
   }
+
+  //id 요휴성검사
   idCheck = e => {
-    const idreg = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])/;
+    const idreg = /^[a-z0-9]{5,15}/g;
     let userId = e.target.value;
     this.setState({ identity: e.target.value });
 
@@ -40,15 +46,16 @@ class Signup extends Component {
       );
     }
   };
+  //id 버튼활성화
   btnChangeColor = () => {
     this.state.idChecked
       ? this.setState({ background: '#6ca437', disabled: false })
       : this.setState({ background: '#d2f7d2', disabled: true });
   };
+  //id 중복체크
   rightId = e => {
     e.preventDefault();
-
-    fetch('http://10.58.0.179:8000/user/idCheck', {
+    fetch('http://10.58.2.234:8000/user/id-check', {
       method: 'POST',
       body: JSON.stringify({
         identity: this.state.identity,
@@ -58,60 +65,118 @@ class Signup extends Component {
 
       .then(result => {
         console.log('결과: ', result);
-        if (result.token) {
-          this.props.history.push('/mainW');
-        } else {
-          this.setState({ isAvailedEmail: '이미 존재하는 아이디 입니다.' });
-        }
+        // if (result.message === 'SUCCESS') {
+        //   this.props.history.push('/login');
+        // } else if (result.message === 'IDENTITY_ALREADY_EXISTS') {
+        //   this.setState({ isAvailedId: '이미 존재하는 아이디 입니다.' });
+        // }
       });
   };
 
-  blur = e => {
-    this.setState({ email: e.target.value });
-    if (e.target.value.includes('@')) {
+  //pw 일치여부확인
+  handleConfirmPassword = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    if (e.target.value !== this.state.rePassword) {
       this.setState({
-        email: e.target.value,
+        isAvailedPassword: '비밀번호가 일치하지 않습니다.',
       });
-    } else {
-      alert('잘못된 이메일 형식입니다.');
+    } else if (e.target.value === '') {
+      this.setState({
+        isAvailedPassword: '',
+      });
+    } else if (e.target.value === this.state.rePassword) {
+      this.setState({
+        isAvailedPassword: '',
+      });
+    }
+  };
+  // repw 일치여부 확인
+  handleConfirmrePassword = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    if (e.target.value !== this.state.password) {
+      this.setState({
+        isAvailedPassword: '비밀번호가 일치하지 않습니다.',
+      });
+    } else if (e.target.value === '') {
+      this.setState({
+        isAvailedPassword: '',
+      });
+    } else if (e.target.value === this.state.password) {
+      this.setState({
+        isAvailedPassword: '',
+        //keyup이벤트 추가(value값 초기화)
+      });
     }
   };
 
-  clickSignup = e => {
-    e.preventDefault();
-    const { identity, password, name, birth_date, phone, email, usableId } =
-      this.state;
-  };
-  clickSignup = e => {
-    e.preventDefault();
-    console.log(this.state.name);
-    fetch('http://10.58.0.179:8000/user/signUp', {
-      method: 'POST',
-      body: JSON.stringify({
-        identity: this.state.identity,
-        password: this.state.password,
-        name: this.state.name,
-        phone: this.state.phone,
-        email: this.state.email,
-        address: this.state.address,
-      }),
-    })
-      .then(response => response.json())
-      .then(result => {
-        if (result.token) {
-          this.props.history.push('/mainW');
-        } else {
-          this.setState({ isAvailedEmail: '이미 존재하는 아이디 입니다.' });
+  //name ,상세주소 phone value값 지정 ,  phone  검사
+  handleOnChange = e => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value,
+      },
+      () => {
+        if (e.target.name === 'phone') {
+          this.PhoneCheck();
+
+          return;
         }
-      });
+      }
+    );
   };
+
+  PhoneCheck = () => {
+    const phonereg = /^\d{3}\d{3,4}\d{4}$/;
+    let userPhone = this.state.phone;
+    this.setState({ phone: userPhone });
+
+    if (false === phonereg.test(userPhone)) {
+      this.setState({ isAvailedPhone: '휴대폰 11자리 입력해주세요' });
+    } else {
+      this.setState({ isAvailedPhone: '' });
+    }
+  };
+
+  //email 형식 검사
+  emailCheck = e => {
+    const emailreg =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    let userEmail = e.target.value;
+    this.setState({ email: e.target.value });
+
+    if (false === emailreg.test(userEmail)) {
+      this.setState({ isAvailedEmail: '올바른 이메일 형식이 아닙니다.' });
+    } else {
+      this.setState({ isAvailedEmail: '' });
+    }
+  };
+
+  // blur = e => {
+  //   this.setState({ email: e.target.value });
+  //   this.state.email &&
+  //     this.setState({
+  //       email: e.target.value,
+  //       isAvailedEmail: '',
+  //     });
+  // } else {
+  //   this.setState({
+  //     email: '',
+  //     isAvailedEmail: '이메일 형식이 맞지 않습니다.',
+  //   });
+  // }
+  // };
+
+  //주소
   handleOpenPost = () => {
     this.setState({
       isDaumPost: true,
     });
   };
-
-  // postcode
+  // daum api 불러오기
   handleAddress = data => {
     let AllAddress = data.address;
     let extraAddress = '';
@@ -128,16 +193,67 @@ class Signup extends Component {
       AllAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
     }
     this.setState({
-      fullAddress: AllAddress,
+      firstAddress: AllAddress,
     });
   };
+
+  //회원가입버튼
+  signUpSummit = e => {
+    e.preventDefault();
+
+    if (this.state.identity === '') {
+      alert('아이디를 입력해주세요');
+    } else if (this.state.password === '') {
+      alert('비밀번호를 입력해주세요');
+    } else if (this.state.name === '') {
+      alert('이름을 입력해주세요');
+    } else if (this.state.phone === '') {
+      alert('휴대폰 번호를 입력해주세요');
+    } else if (this.state.email === '') {
+      alert('이메일을 입력해주세요');
+    } else if (this.state.address === '') {
+      alert('주소를 입력해주세요');
+    } else {
+      this.clickSignup();
+    }
+  };
+
+  //백엔드 통신
+  clickSignup = e => {
+    e.preventDefault();
+
+    // const { identity, password, name, phone, email, address } = this.state;
+  };
+  clickSignup = e => {
+    console.log(this.state.address);
+    fetch('http://10.58.2.234:8000/user/sign-up', {
+      method: 'POST',
+      body: JSON.stringify({
+        identity: this.state.identity,
+        password: this.state.password,
+        name: this.state.name,
+        phone: this.state.phone,
+        email: this.state.email,
+        address: this.state.firstAddress + this.state.secondAddress,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log('결과: ', result);
+        // if (result.message === 'SUCCESS') {
+        //   this.props.history.push('/login');
+        // } else if (result.message === 'IDENTITY_ALREADY_EXISTS') {
+        //   this.setState({ isAvailedId: '이미 존재하는 아이디 입니다.' });
+        // }
+      });
+  };
+
   render() {
-    const { isModalShow, isModalClose } = this.props;
-    const { name, phone, email, address, isDaumPost, fullAddress } = this.state;
+    // const { isModalShow, isModalClose } = this.props;
+    const { isDaumPost, firstAddress } = this.state; //name, phone, email, address,
     // DaumPostCode style
     const width = 505;
     const height = 420;
-    // const position = 'absoulte';
     const modalStyle = {
       position: 'absolute',
       // top: 0,
@@ -187,70 +303,92 @@ class Signup extends Component {
                 <input
                   id="password"
                   name="password"
+                  onChange={this.handleConfirmPassword}
                   type="password"
                   placeholder="비밀번호 (8~18 자, 대소문자 숫자 특수문자 모두 포함)"
                 />
-                <div>{this.state.isAvailedPassword}</div>
+
                 <input
                   id="rePassword"
+                  name="rePassword"
+                  onChange={this.handleConfirmrePassword}
                   type="password"
                   placeholder="비밀번호확인"
                 />
+                <p className="errMsg">{this.state.isAvailedPassword}</p>
               </div>
               <div className="member_Info">
-                <input className="name" type="text" placeholder="이름" />
+                <input
+                  className="name"
+                  name="name"
+                  onChange={this.handleOnChange}
+                  type="text"
+                  placeholder="이름"
+                />
                 <input
                   className="phone"
                   name="phone"
+                  onChange={this.handleOnChange}
                   type="number"
                   placeholder="휴대폰번호 (-제외 숫자만입력)"
                 />
+                <p className="errMsg">{this.state.isAvailedPhone}</p>
                 <input
                   className="email"
                   name="email"
-                  onBlur={this.blur}
+                  onChange={this.emailCheck}
                   type="text"
                   placeholder="이메일"
                 />
+                <p className="errMsg">{this.state.isAvailedEmail}</p>
               </div>
-
-              <div className="cellFirst">
-                <input
-                  placeholder="주소"
-                  className="main_Address"
-                  value={fullAddress}
-                />
-                <button
-                  className="btn-serch"
-                  type="button"
-                  onClick={this.handleOpenPost}
-                >
-                  주소 찾기
-                </button>
-              </div>
-              {isDaumPost && (
-                <DaumPostCode
-                  onComplete={this.handleAddress}
-                  autoClose
-                  width={width}
-                  height={height}
-                  style={modalStyle}
-                  isDaumPost={isDaumPost}
-                />
-              )}
-
               <div className="addressBox">
-                <input
-                  type="text"
-                  name="address"
-                  placeholder="상세주소"
-                  className="basic_Address"
-                  onChange={this.handleInput}
+                <div className="cellFirst">
+                  <input
+                    placeholder="주소"
+                    className="main_Address"
+                    value={firstAddress}
+                  />
+                  <button
+                    className="btn-serch"
+                    type="button"
+                    onClick={this.handleOpenPost}
+                  >
+                    주소 찾기
+                  </button>
+                  {isDaumPost && (
+                    <DaumPostCode
+                      onComplete={this.handleAddress}
+                      autoClose
+                      width={width}
+                      height={height}
+                      style={modalStyle}
+                      isDaumPost={isDaumPost}
+                    />
+                  )}
+                </div>
+
+                <div className="spec_Address">
+                  <input
+                    type="text"
+                    name="secondAddress"
+                    placeholder="상세주소"
+                    className="second_Address"
+                    onChange={this.handleOnChange}
+                    // onKeyUp={this.handleInput}
+                  />
+                </div>
+              </div>
+              <div>
+                <img
+                  classname="recom_Img"
+                  alt="가입혜택"
+                  src="/images/recom.png"
                 />
               </div>
 
               <div className="finish">
-                <button className="summit" onClick={this.clickSignup}>
+                <button className="summit" onClick={this.signUpSummit}>
                   가입하기
                 </button>
               </div>
