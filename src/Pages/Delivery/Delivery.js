fe2d240654cb84dd697606a24fe251db567c7aab
JOHNKIM-KK <76652614+JOHNKIM-KK@users.noreply.github.com>
2021-06-13 16:10:00 +0900

    add:주소정보창 레이아웃 구현

--- src/Pages/Delivery/Delivery.js
+++ src/Pages/Delivery/Delivery.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Delivery.scss';
 
 class Delivery extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      UserName: '',
+      UserPhone: '',
+    };
+  }
   render() {
     return (
       <div className="delivery">
@@ -22,11 +29,11 @@ class Delivery extends React.Component {
           <div className="user-info-container">
             <div>
               <label htmlFor="orderUserName">보내시는 분</label>
-              <input id="orderUserName"></input>
+              <input id="orderUserName" name="orderUserName"></input>
             </div>
             <div>
               <label htmlFor="orderUserPhone">연락처</label>
-              <input id="orderUserPhone"></input>
+              <input id="orderUserPhone" name="orderUserPhone"></input>
             </div>
           </div>
 
@@ -44,6 +51,25 @@ class Delivery extends React.Component {
               <div>
                 <input type="radio" id="new" name="address"></input>
                 <label for="new">직접입력</label>
+              </div>
+            </div>
+            <div className="address-info">
+              <div className="user-info">
+                <input></input>
+                <input></input>
+              </div>
+              <div className="address-input">
+                <div className="address-zip">
+                  <input></input>
+                  <button>주소찾기</button>
+                </div>
+                <div className="detailed-address">
+                  <input></input>
+                  <input></input>
+                  <span>
+                    배송을 위해 고객님의 주소가 맞는지 다시한번 확인해주세요
+                  </span>
+                </div>
               </div>
             </div>
           </div>
