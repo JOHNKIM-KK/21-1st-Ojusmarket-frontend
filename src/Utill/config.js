0efa76c779b22004fd1b827a55481eae8a477ec1
김민기 <mingikim@gimmingiui-MacBookAir.local>
2021-06-20 14:37:47 +0900

    Modified: url수정

--- src/Utill/config.js
+++ src/Utill/config.js
@@ -1,7 +1,7 @@
 // const MAIN_LIST = 'http://10.58.6.166:8000';
 // const SIGNUP_URL = 'http://10.58.7.21:8000';
 // const LOGIN_URL = 'http://10.58.7.179:8000';
-const BASE_URL = 'http://10.58.1.164:8000';
+const BASE_URL = 'http://18.117.10.173:8000';
 
 export const GET_PRODUCT_API = `${BASE_URL}`;
 export const GET_DELIVERY_API = `${BASE_URL}/orders`;
